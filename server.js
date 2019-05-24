5f1def99920579d62d2af2e388fa1b23b6101b66
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-05-24 12:07:53 +0200

    ok

--- server.js
+++ server.js
@@ -8,18 +8,24 @@ app.use(function (req, res, next) {
     next();
 });
 
-const htdocs = '.';
+const htdocs = '../quizz/dist/quizz';
 app.use(express.static(htdocs));
 app.use(serveIndex(htdocs, {
     icons: true
 }));
+
+app.use('/', (req, res, next) => {
+    res.sendFile('./quizz/index.html', {
+        root: __dirname,
+    });
+});
 
 app.use(function (req, res, next) {
     console.log('404: Page not Found', req.url);
     next();
 });
 
-const port = 8000;
+const port = 5000;
 app.listen(port, function () {
     console.log('server started on port ' + port);
 });

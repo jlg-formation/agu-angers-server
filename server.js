5a7971205d4551d5e69245441b76425efad84db4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-05-27 16:03:12 +0200

    ok

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const serveIndex = require('serve-index');
+const cors = require('cors');
 
 const app = express();
+
+app.use(express.json());
+
+app.use(cors());
 
 app.use(function (req, res, next) {
     console.log('req.url', req.url);
     next();
+});
+
+let quizzlist = {};
+app.get('/ws/quizzlist', (req, res, next) => {
+    res.json(quizzlist);
+});
+
+app.post('/ws/quizzlist', (req, res, next) => {
+    quizzlist = req.body;
+    res.status(204).end();
 });
 
 const htdocs = '../quizz/dist/quizz';
